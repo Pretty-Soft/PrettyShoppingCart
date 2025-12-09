b4d0ae35997fcd3f2790a94207a9e51a4240073c
Md.Zahidul Islam <zs.islam93@gmail.com>
2025-12-09 10:55:44 +0600

    Added Splash screen

--- src/Presentation/Nop.Web.Framework/Infrastructure/PublicWidgetZones.cs
+++ src/Presentation/Nop.Web.Framework/Infrastructure/PublicWidgetZones.cs
@@ -233,4 +233,5 @@ public static partial class PublicWidgetZones
     public static string VendorInfoTop => "vendor_info_top";
     public static string WishlistBottom => "wishlist_bottom";
     public static string WishlistTop => "wishlist_top";
+    public static string SplashHome => "splashScreen";
 }
